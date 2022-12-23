a5c620f9ce18df67eeeb45eda42d23f10dc0a7e7
MorganIII <mosaab.morgan@gmail.com>
2022-12-23 07:45:48 +0200

    new

--- src/main/java/com/morgan/video_streaming/rest/VideoController.java
+++ src/main/java/com/morgan/video_streaming/rest/VideoController.java
@@ -1,16 +1,10 @@
 package com.morgan.video_streaming.rest;
-
-
 import com.morgan.video_streaming.service.VideoService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -32,6 +26,7 @@ public class VideoController {
                 .ok(new ByteArrayResource(videoService.getVideo(name).getVideo()));
     }
 
+    //send the name of the videos to the frontend.
     @GetMapping("all")
     public ResponseEntity<List<String>> getAllVideoNames(){
         return ResponseEntity
